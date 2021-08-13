4e21e6b0a90489da24c45c480ca045225d9a525a
Karma Blackshaw <irenesejah29@gmail.com>
2021-08-13 21:34:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="min-h-screen">
+    <article>
+      <section>
+        <div>
+          <app-navbar />
+
+          <router-view />
+        </div>
+      </section>
+    </article>
+  </main>
+</template>
+
+<script>
+
+import AppNavbar from '@/components/app/AppNavbar'
+
+export default {
+  name: 'App',
+
+  components: {
+    AppNavbar
+  }
+}
+</script>
